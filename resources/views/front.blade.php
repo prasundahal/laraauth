3449b6e7327dbf6d88c87e50820f885e8a370a22
Prasun Dahal <prasundahal@gmail.com>
2021-04-09 22:37:43 +0545

    formedited

--- resources/views/front.blade.php
+++ resources/views/front.blade.php
@@ -33,49 +33,66 @@
                 <div class="card-heading"></div>
                 <div class="card-body">
                     <h2 class="title">Registration Info</h2>
-                    <form method="POST">
+                    <form action="{{ route('frontcv.store') }}" method="POST">
                         <div class="input-group">
                             <input class="input--style-1" type="text" placeholder="NAME" name="name">
                         </div>
                         <div class="row row-space">
+                        <div class="input-group">
+                            <input class="input--style-1" type="text" placeholder="Email" name="email">
+                        </div>
+                        <div class="input-group">
+                            <input class="input--style-1" type="text" placeholder="Phone" name="phone">
+                        </div>
+                        </div>
+                        <div class="row row-space">
                             <div class="col-2">
                                 <div class="input-group">
-                                    <input class="input--style-1 js-datepicker" type="text" placeholder="BIRTHDATE" name="birthday">
+                                    <input class="input--style-1 js-datepicker" type="text" placeholder="EXPERTIES" name="exp">
                                     <i class="zmdi zmdi-calendar-note input-icon js-btn-calendar"></i>
                                 </div>
                             </div>
                             <div class="col-2">
                                 <div class="input-group">
                                     <div class="rs-select2 js-select-simple select--no-search">
-                                        <select name="gender">
-                                            <option disabled="disabled" selected="selected">GENDER</option>
-                                            <option>Male</option>
-                                            <option>Female</option>
+                                        <select name="social">
+                                            <option disabled="disabled" selected="selected">WHERE ARE U?</option>
+                                            <option>Fb</option>
+                                            <option>Ins</option>
                                             <option>Other</option>
                                         </select>
                                         <div class="select-dropdown"></div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="input-group">
-                            <div class="rs-select2 js-select-simple select--no-search">
-                                <select name="class">
-                                    <option disabled="disabled" selected="selected">CLASS</option>
-                                    <option>Class 1</option>
-                                    <option>Class 2</option>
-                                    <option>Class 3</option>
-                                </select>
-                                <div class="select-dropdown"></div>
-                            </div>
-                        </div>
-                        <div class="row row-space">
                             <div class="col-2">
                                 <div class="input-group">
-                                    <input class="input--style-1" type="text" placeholder="REGISTRATION CODE" name="res_code">
+                                    <div class="rs-select2 js-select-simple select--no-search">
+                                        <select name="yearexp">
+                                            <option disabled="disabled" selected="selected">yearexp</option>
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                        </select>
+                                        <div class="select-dropdown"></div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <input class="input--style-1" type="text" placeholder="PRICE" name="price">
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <input class="input--style-1" type="file" placeholder="cv" name="image">
+                                </div>
+                            </div>
+                        </div>
+
                         <div class="p-t-20">
                             <button class="btn btn--radius btn--green" type="submit">Submit</button>
                         </div>
@@ -95,7 +112,7 @@
     <!-- Main JS-->
     <script src="front/js/global.js"></script>
 
-</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+</body>
 
 </html>
 <!-- end document-->
