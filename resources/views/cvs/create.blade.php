19967dff1b9a08f727809914970f4d63844dd1cd
Sudip Raj Kunwar <sudip.kunwar9898@gmail.com>
2021-05-02 19:25:24 +0545

    #form

--- resources/views/cvs/create.blade.php
+++ resources/views/cvs/create.blade.php
@@ -1,16 +1,6 @@
 @extends('cvs.layout')
 
 @section('content')
-<div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>Add Your Details</h2>
-        </div>
-        <div class="pull-right">
-            {{-- <a class="btn btn-primary" href="{{ route('cvs.index') }}"> Back</a> --}}
-        </div>
-    </div>
-</div>
 
 @if ($errors->any())
     <div class="alert alert-danger">
@@ -23,10 +13,10 @@
     </div>
 @endif
 
-<form action="{{ route('cvs.store') }}" enctype="multipart/form-data" method="POST">
-    @csrf
+{{-- <form action="{{ route('cvs.store') }}" enctype="multipart/form-data" method="POST">
+    @csrf --}}
 
-     <div class="row">
+     {{-- <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>email:</strong>
@@ -54,7 +44,6 @@
                     <option>Ins</option>
                     <option>Other</option>
                 </select>
-                {{-- <input type="text" name="social" class="form-control" placeholder="social"> --}}
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
@@ -92,7 +81,67 @@
         <div class="col-xs-12 col-sm-12 col-md-12 text-center">
                 <button type="submit" class="btn btn-primary">Submit</button>
         </div>
+    </div> --}}
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <!-- form card login -->
+            <div class="card card-outline-secondary">
+                <div class="card-header">
+                    <h3 class="mb-0">Add Your Details</h3>
+                </div>
+                <div class="card-body" style="padding: 1.25rem">
+                    <form action="{{ route('cvs.store') }}" enctype="multipart/form-data" method="POST" autocomplete="off" class="form" id="formLogin" name="formLogin" role="form">
+                        @csrf
+                            <div class="form-group">
+                                <label for="Username">Username</label> 
+                                <input type="text" class="form-control" name="Username" placeholder="Username">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="Email">Email</label> 
+                                <input type="text" class="form-control" name="Email"  placeholder="Email">
+                            </div>
+                        
+                            <div class="form-group">
+                                <label for="Phone">Phone</label> 
+                                <input type="number" class="form-control" name="Phone"  placeholder="Phone">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="Social">Social</label><br/>
+                                <select name="social" class="form-select form-select-lg mb-3">
+                                    <option disabled="disabled" selected="selected">WHERE ARE U?</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </select>
+                             
+                            </div>
+
+                        
+                            <div class="form-group">
+                                <label for="Experience">Experience</label> 
+                                <input type="text" class="form-control" name="Experience" placeholder="Experience">
+                            </div>
+
+                        
+                            <div class="form-group">
+                                <label for="Price">Price</label> 
+                                <input type="text" name="Price" class="form-control" placeholder="price">
+                            </div>
+
+                        
+                            <div class="form-group">
+                                <label for="formFile" class="form-label">Image</label>
+                                <input class="form-control" type="file" id="formFile">                                  
+                            </div>
+                            
+                        
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div><!--/card-block-->
+            </div><!-- /form card login -->
+        </div>
     </div>
 
-</form>
 @endsection
